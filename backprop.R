ad84cd7bc89ffb28f0f39711d7896c5e227d458f
Zach <drumert@users.noreply.github.com>
2016-05-19 11:24:01 -0600

    initial backprop file

---
+++ backprop.R
@@ -0,0 +1,5 @@
+##Back propogation
+
+#in order to back propogate we use the delta method. Finding a delta value for every node in
+#a layer. We begin by calculating delta values, and subsequent partial derivatives of overall cost relative to 
+#a particular weight.
