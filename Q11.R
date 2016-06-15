fef389b0119d3331f477b08a5992420c8014c15f
Zach <drumert@users.noreply.github.com>
2016-06-15 11:23:39 -0600

    working 11 and 21 game player

--- Q11.R
+++ Q11.R
@@ -1,104 +1,132 @@
-#Q learning on 22 count down
 options(scipen=999)
-Q1 <- matrix(0,nrow=11,ncol=3) #matrix initializing long term values for player 1
-Q2 <- matrix(0,nrow=11,ncol=3)
+Q <- matrix(0,nrow=21,ncol=3)#,nrow=11)
 r <- rbind(
-  c(100,-100,-100), #if 1 remains then playing 1 wins and otherwise loses
-  #c(-100,100,-100), #if 2 remain then playing 2 wins and otherwise loses
-  c(0,100,-100) #test 2
-  #c(-100,-100,100), #if 3 remain then playing 3 wins and otherwise loses
-  c(0,100,-100), #test 3
-  #c(-100,-100,-100), #if 4 remain then a loss is inevitable
-  c(0,0,0),#test for 4
-  #c(0,-100,-100), #if 5 remain then playing 2 or 3 leads to loss
-  c(0,0,0),#test for 5
-  #c(0,0,-100), #if 6 remain then playing 3 leads to loss
-  c(0,0,0),#test for 6
-  c(0,0,0), #beyond this point nothing is obviously deterministic (but can be derived)
+  c(100,-100,-100),
+  c(0,100,-100),
+  c(0,0,100),
   c(0,0,0),
   c(0,0,0),
   c(0,0,0),
   c(0,0,0),
+  c(0,0,0),
+  c(0,0,0),
+  c(0,0,0),
+  c(0,0,0) , c(0,0,0),c(0,0,0),c(0,0,0),c(0,0,0),c(0,0,0),c(0,0,0),c(0,0,0),c(0,0,0),c(0,0,0),c(0,0,0)
 )
 
-Qchoice <- function(total,Q,ep)
+
+
+legalRandom <- function(total)
 {
-  if(runif(1)>ep) #equivalent to flipping a coin. This is the greedy choice
+  if(total <=3)
   {
-    choice <- match(max(Q[total,]),Q[total,]) #makes the choice with the highest Q value given the current state
+    return(sample(1:total,1))
   }
   else
   {
-    choice <- sample(1:3,1) #makes random choice
+    return(sample(1:3,1))
   }
-  return(choice)
 }
 
-is.special.choice <- function(choice,total)
+checkGameOver <- function(total)
 {
-  if(total-choice <= 0) {return(TRUE)}
-  else {return(FALSE)}
+  if(total==0)
+  {
+    return(TRUE)
+  }
+  else
+  {
+    return(FALSE)
+  }
 }
-learn22 <- function(Q1,Q2=NULL,r,ep,lrate=.9,disc=.5,games,humanP2)
+
+learn11 <- function(Q,r,ep,lrate,disc,games,start)
 {
   for(i in 1:games)
   {
-  cat("Game ",i," of ",games,"\n")
-  total <- 11
-  while(TRUE)
-  {
-    choice <- Qchoice(total,Q1,ep) #player1 go
-    reward <- r[total,choice]
-    if(is.special.choice(choice,total)) 
+    cat("\nGame ",i," of ",games,"\n")
+    total <- start
+    while(TRUE)#This plays one game
     {
-      signshift <- (2)*sign(total-choice)+1#differentiates between winning at 0 and losing
-      Q1[total,choice] <- Q1[total,choice]+lrate*(reward+signshift*disc*100-Q1[total,choice])
-      cat("Player 1 (computer) has chosen: ",choice," The total remaining is: ",total-choice,"\nPlayer 1 (computer) wins!\n")
-      break
-    }
-    else
-    {#this is the case when the game is played on
-      Q1[total,choice] <- Q1[total,choice]+lrate*(reward+disc*max(Q1[total-choice,])-Q1[total,choice])
-    }
-    total <- total - choice
-    cat("Player 1 (computer) has chosen: ",choice," The total remaining is: ",total,"\n")
-    
-    #now player two goes
-    if(humanP2 == TRUE)
-    {
-      choice <- as.numeric(readline("Player 2 select 1, 2, or 3: "))
-      total <- total - choice
-      cat("The total remaining is: ",total,"\n")
-      if(total == 0)
+      if(runif(1)>ep)#greedy step
       {
-        cat("Player 2 wins!\n")
+        choiceCom <- match(max(Q[total,]),Q[total,]) #finds the highest valued next state
+      }
+      else #epsilon random step
+      {
+        choiceCom <- legalRandom(total) #randomly selects the next legal state
+      }
+      cat("Qcom chooses: ",choiceCom," total remaining: ",total-choiceCom,"\n")
+      reward <- r[total,choiceCom]
+      if(checkGameOver(total-choiceCom))
+      {#if the game is over immediatly it means the computer won and learns positivity immediatly
+        Q[total,choiceCom] <- Q[total,choiceCom]+lrate*(reward+disc*(100)-Q[total,choiceCom])
+        cat("Q wins!\n")
         break
       }
-    }
-    else
-    {
-      choice <- Qchoice(total,Q2,ep) #computer [player 2 goes]
-      reward <- r[total,choice]
-      if(is.special.choice(choice,total)) 
-      {
-        signshift <- (2)*sign(total-choice)+1#differentiates between winning at 0 and losing
-        Q2[total,choice] <- Q2[total,choice]+lrate*(reward+signshift*disc*100-Q2[total,choice])
-        cat("Player 2 (computer) has chosen: ",choice," The total remaining is: ",total-choice,"\nPlayer 2 (computer) wins!\n")
-        break
-      }
+
+      #before, we had set things up so that it would learn right after it played,
+      #but in fact you can only learn something in this game after your opponent plays.
+      #Therefore we will include another step to change state again before it learns.
+      #We will have it play a random player to start
       else
-      {#this is the case when the game is played on
-        Q2[total,choice] <- Q2[total,choice]+lrate*(reward+disc*max(Q2[total-choice,])-Q2[total,choice])
+      {
+        choiceRand <- legalRandom(total-choiceCom)
+        cat("Rand chooses: ",choiceRand," total remaining: ",total-choiceRand-choiceCom,"\n")
+        if(checkGameOver(total-choiceCom-choiceRand))
+        {#if the opponent ends the game, the computer learns negativity immediatly
+          Q[total,choiceCom] <- Q[total,choiceCom]+lrate*(reward+disc*(-100)-Q[total,choiceCom])
+          cat("Rand wins!\n")
+          break
+        }
+        else
+        {#otherwise it will learn about its choice based on where it ended up 
+          Q[total,choiceCom] <- Q[total,choiceCom]+lrate*(reward+disc*max(Q[total-choiceCom-choiceRand,])-Q[total,choiceCom])
+        }
+        total <- total-choiceCom-choiceRand #now we end and repeat till the game is over
       }
-      total <- total - choice
-      cat("Player 2 (computer) has chosen: ",choice," The total remaining is: ",total,"\n")
     }
   }
-  }
-  assign("Q1",Q1,envir=.GlobalEnv)
-  assign("Q2",Q2,envir=.GlobalEnv)
+  assign("Q",Q,envir=.GlobalEnv)
 }
 
-#need to resolve this. It has converged properly in the past, but not now.
+learn11(Q,r, ep=.1,lrate=.1,disc=1,games=20000,start=21)
 
-
+play11 <- function(Q,comFirst=TRUE,start=21)
+{
+  total <- start
+  cat("Total: ", total,"\n")
+  if(comFirst)
+  {
+    choice <- match(max(Q[total,]),Q[total,])
+    total <- total - choice
+    cat("Computer chooses: ",choice," Total remining: ",total,"\n")
+  }
+  while(TRUE)
+  {
+    while(TRUE)
+    {
+      pchoice <- as.numeric(readline("Player Select 1, 2, or 3: "))
+      if((pchoice != 1 && pchoice != 2 && pchoice !=3) || total-pchoice < 0)
+      {
+        pchoice <- as.numeric(readLines("Invalid Entry, Please select a legal move: "))
+      }
+      else {break}
+    }
+    total <- total - pchoice
+    cat("Total remaining: ",total,"\n")
+    if(checkGameOver(total))
+    {
+      cat("Player Wins!!!\n")
+      break
+    }
+    choice <- match(max(Q[total,]),Q[total,])
+    total <- total - choice
+    cat("Computer chooses: ",choice," Total remining: ",total,"\n")
+    if(checkGameOver(total))
+    {
+      cat("Computer Wins!!!\n")
+      break
+    }
+  }
+}
