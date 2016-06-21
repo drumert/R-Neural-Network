e2ed6c148b9a90e9a688e4d06665972a2694d54e
Zach <drumert@users.noreply.github.com>
2016-06-21 12:08:10 -0600

    ineff tic tac

--- Qtictac.R
+++ Qtictac.R
@@ -11,8 +11,8 @@ generateGame <- function()
   assign("board",game,envir = .GlobalEnv)
 }
 
-#check for win, return either a win or a loss or a true (win for X)
-score <- function(board)
+#check for win, return either a win or a loss (win for X = TRUE) #this is the Reward function
+reward <- function(board)
 {
   possibles <- numeric()
   possibles[1] <- sum(board[1:3]) #top row
@@ -55,6 +55,96 @@ showPossibleMoves <- function(board)
   return(out)
 }
 
-Qlib <- data.frame(q11=0,q12=0,q13=0,q21=0,q22=0,q23=0,q31=0,q32=0,q33=0,r=0)
+opPossibleMoves <- function(board)
+{
+  out <- list()
+  cnt <- 0
+  for(i in 1:9)
+  {
+    if(board[i]==0)
+    {
+      board[i] <- -1
+      cnt <- cnt + 1
+      out[[cnt]] <- board
+      board[i] <- 0
+    }
+  }
+  return(out)
+}
+Qindex <- function(board,Qlib)
+{
+  asis <- 1:9
+  rot180 <- 9:1
+  rot90 <- c(7,4,1,8,5,2,9,6,3)
+  rot270 <- c(3,6,9,2,5,8,1,4,7)
+  horFlip <- c(7,8,9,4,5,6,1,2,3)
+  vertFlip <- c(3,2,1,6,5,4,9,8,7)
+  diagFlip <- c(1,4,7,2,5,8,3,6,9)
+  crossdiagFlip <- c(9,6,3,8,5,2,7,4,1)
+  I <- 0
+  for(i in 1:length(Qlib))
+  {
+    if(Qlib[[i]]$brd == board[asis]){return(i)} #as is
+    else if(Qlib[[i]]$brd == board[rot180]){return(i)} #180 degree rotate
+    else if(Qlib[[i]]$brd == board[rot90]){return(i)} #90 degree rotate right
+    else if(Qlib[[i]]$brd == board[rot270]){return(i)}
+    else if(Qlib[[i]]$brd == board[horFlip]){return(i)}
+    else if(Qlib[[i]]$brd == board[vertFlip]){return(i)}
+    else if(Qlib[[i]]$brd == board[diagFlip]){return(i)}
+    else if(Qlib[[i]]$brd == board[crossdiagFlip]){return(i)}
+    I <- i #if we get all the way here then we need to add the board configuration to Qlib
+  }
+  Qlib[[I+1]] <- list(brd = board, Q = 0)
+  assign("Qlib",Qlib,envir = .GlobalEnv)
+  return(I+1)
+}
+
+Qlib <- list()
+Qlib[[1]] <- list(brd = c(0,0,0,0,0,0,0,0,0),Q=0)
+
+learnTicTac <- function(Qlib,ep=.1,lrate=.1,disc=1,games)
+{
+  for(i in 1:games)
+  {#Here is where we iterate a ton of games
+    generateGame()
+    while(TRUE) #this loop goes on for the duration of one game
+    {
+      poss <- showPossibleMoves(board)#To start, computer goes first and assesses possibilities
+      #Now we decide how to chooce a move:
+      if(runif(1) < ep)#random option
+      {
+        choice <- sample(1:length(poss),1)
+        boardCom <- poss[choice]
+      }
+      else #greedy option
+      {
+        possQ <- numeric()
+        for(i in 1:length(poss))
+        {#here we grab the Q values for each possible move
+          possQ[i] <- Qlib[[Qindex(poss[[i]],Qlib)]]$Q
+        }
+        boardCom <- poss[[match(max(possQ),possQ)]] #then we take the highest one
+      }
+      #Before we go on we check for a win
+      if(reward(boardCom) > 0)
+      {
+        Qlib[[Qindex(boardCom)]]###########
+        break
+      }
+      #Now that a move is chosen, the oponent picks randomly
+      opPoss <- opPossibleMoves(boardCom)
+      boardOp <- sample(1:length(opPass),1)
+      boardRand <- opPoss[[boardOp]]
+      
+      #now we check for a loss
+      if(reward(boardRand) < 0)
+      {
+        break
+      }
+      #Now we update
+      
+    }
+  }
+}
 
 
---
+++ inefQTT.R
@@ -0,0 +1,248 @@
+#Here we build an inefficient Q learning setup where we don't check for symetry
+
+#note that Q values usually are part of state-action pairs, but in our case, we consider a state to state
+#model where the Q value represents the value of ARRIVING there
+reward <- function(board)
+{
+  possibles <- numeric()
+  possibles[1] <- sum(board[1:3]) #top row
+  possibles[2] <- sum(board[4:6]) #mid row
+  possibles[3] <- sum(board[7:9]) #bottom row
+  possibles[4] <- board[1] + board[4] + board[7] #first col
+  possibles[5] <- board[2] + board[5] + board[8] #second col
+  possibles[6] <- board[3] + board[6] + board[9] #third col
+  possibles[7] <- board[1] + board[5] + board[9] #true diag
+  possibles[8] <- board[3] + board[5] + board[7] #back diag
+  if(any(possibles == 3))
+  {
+    return(100)
+  }
+  else if(any(possibles == -3))
+  {
+    return(-100)
+  }
+  else
+  {
+    return(-1)
+  }
+}
+
+showPossibleMoves <- function(board)
+{
+  out <- list()
+  cnt <- 0
+  for(i in 1:9)
+  {
+    if(board[i]==0)
+    {
+      board[i] <- 1
+      cnt <- cnt + 1
+      out[[cnt]] <- board
+      board[i] <- 0
+    }
+  }
+  return(out)
+}
+
+opPossibleMoves <- function(board)
+{
+  out <- list()
+  cnt <- 0
+  for(i in 1:9)
+  {
+    if(board[i]==0)
+    {
+      board[i] <- -1
+      cnt <- cnt + 1
+      out[[cnt]] <- board
+      board[i] <- 0
+    }
+  }
+  return(out)
+}
+
+addtoQ <- function(board,Q)
+{
+  I <- 0
+  for(i in 1:length(Q))
+  {
+    if(all(Q[[i]]$board == board))
+    {
+      return(Q)
+    }
+    I <- i
+  }
+  Q[[I+1]] <- list(board = board, q = 0)
+  return(Q)
+}
+
+Qind <- function(board,Q)
+{
+  for(i in 1:length(Q))
+  {
+    if(all(Q[[i]]$board == board))
+    {
+      return(i)
+    }
+  }
+}
+
+random.choice <- function(poss)
+{
+  choice <- sample(1:length(poss),1)
+  return(poss[[choice]])
+}
+
+getQ <- function(board,Q)
+{#future effeciency idea is to merge this function with the Q adder function
+  for(i in 1:length(Q))
+  {
+    if(all(Q[[i]]$board == board))
+    {
+      return(Q[[i]]$q)
+    }
+  }
+}
+
+Q <- list()
+Q[[1]] <- list(board = c(0,0,0,0,0,0,0,0,0), q = 0)
+learnTTinef <- function(Q,ep=.1,lrate=.1,disc=1,games)
+{
+  for(i in 1:games)#play many rounds
+  {
+    board <- c(0,0,0,0,0,0,0,0,0) #newgame
+    while(TRUE)#Play a single game
+    {#computer goes first
+      poss <- showPossibleMoves(board) #Observe the possible moves
+      if(runif(1) > ep) #decide how to play
+      {#greedy play
+        #check to see if the possibilities have Q values and add them if they don't
+        for(i in 1:length(poss))
+        {
+          Q <- addtoQ(poss[[i]],Q) #if already contained then it returns original Q
+        }
+        #Now obtain the Q values
+        possQs <- vector()
+        for(i in 1:length(poss))
+        {
+          possQs[i] <- getQ(poss[[i]],Q)
+        }
+        boardCom <- poss[[match(max(possQs),possQs)]] #Choose highest board Q value
+      }
+      else
+      {#random exploration play
+        boardCom <- random.choice(poss)
+      }
+      #Before going on we need to check for a win OR A DRAW
+      if(reward(boardCom) == 100)
+      {
+        cat("Computer Wins!!\n")
+        loc <- Qind(boardCom,Q)
+        Q[[loc]]$q <- Q[[loc]]$q + lrate*(reward(boardCom)+disc*(100)-Q[[loc]]$q)
+        break
+      }
+      if(all(boardCom != 0))
+      {
+        cat("Cats Game...\n")
+        loc <- Qind(boardCom,Q)
+        Q[[loc]]$q <- Q[[loc]]$q + lrate*(0-Q[[loc]]$q)
+        break
+      }
+      #Now that board is chosen we have the oponent choose randomly
+      opPoss <- opPossibleMoves(boardCom)
+      boardOp <- opPoss[[sample(1:length(opPoss),1)]]
+      for(i in 1:length(opPoss))
+      {
+        Q <- addtoQ(opPoss[[i]],Q) #if already contained then it returns original Q
+      }
+      #Now we need to check if the oponent has won
+      if(reward(boardOp) == -100)
+      {
+        cat("Random Wins!!\n")
+        loc <- Qind(boardCom,Q)
+        Q[[loc]]$q <- Q[[loc]]$q + lrate*(reward(boardOp)+disc*(-100)-Q[[loc]]$q)
+        break
+      }
+      #Now that the oponent has chosen, we learn
+      loc <- Qind(boardCom,Q)
+      #get max Q from boardOp
+      tempPoss <- showPossibleMoves(boardOp)
+      for(i in 1:length(tempPoss))
+      {
+        Q <- addtoQ(tempPoss[[i]],Q) #if already contained then it returns original Q
+      }
+      tempQs <- vector()
+      for(i in 1:length(tempPoss))
+      {
+        tempQs[i] <- getQ(tempPoss[[i]],Q)
+      }
+      Q[[loc]]$q <- Q[[loc]]$q + lrate*(reward(boardCom)+disc*max(tempQs)-Q[[loc]]$q)
+      board <- boardOp #now we begin again with the oponents board chocie as our current board
+    }
+  }
+  assign("Q",Q,envir=.GlobalEnv)
+}
+
+learnTTinef(Q,ep=.5,lrate=.1,disc=1,games=1000)
+
+printBoard <- function(board)
+{
+  char <- character()
+  for(i in 1:9)
+  {
+    if(board[i]==1)
+    {
+      char[i] <- "X"
+    }
+    else if(board[i] == -1)
+    {
+      char[i] <- "O"
+    }
+    else
+    {
+      char[i] <- " "
+    }
+  }
+  
+  cat(char[1],"|",char[2],"|",char[3],"\n_________\n")
+  cat(char[4],"|",char[5],"|",char[6],"\n_________\n")
+  cat(char[7],"|",char[8],"|",char[9],"\n")
+}
+
+playTT <- function(Q)
+{
+  board <- c(0,0,0,0,0,0,0,0,0)
+  while(TRUE)
+  {
+    poss <- showPossibleMoves(board) #Observe the possible moves
+    possQs <- vector()
+    for(i in 1:length(poss))
+    {
+      possQs[i] <- getQ(poss[[i]],Q)
+    }
+    board <- poss[[match(max(possQs),possQs)]] #Choose highest board Q value
+    
+    cat("Computers Move\n")
+    printBoard(board)
+    if(reward(board) > -1)
+    {
+      cat("Computer Wins!!\n")
+      break
+    }
+    if(all(board != 0))
+    {
+      cat("Cats Game...\n")
+      break
+    }
+    
+    cat("Player Move\n")
+    choice <- as.numeric(readline("Select the number of the slot you wish to play: "))
+    board[choice] <- -1
+    printBoard(board)
+    if(reward(board) < -1)
+    {
+      cat("Player Wins!!\n")
+      break
+    }
+  }
+}
