2f85f62e21665690608d2594d384354ee0c9d0a6
Zach <drumert@users.noreply.github.com>
2016-06-28 13:05:29 -0600

    multiple regression on log transformed time

--- Q11Reg.R
+++ Q11Reg.R
@@ -85,10 +85,10 @@ convTime <- function(nS,nA,ep=.1,lrate=.1,disc=1)
 Qdata <- data.frame(matrix(ncol=3))
 colnames(Qdata) <- c("States","Actions","Time")
 Qc <- 0
-for(i in 11:30)
+for(i in 11:50)
 {
   print(i)
-  for(j in 2:6)
+  for(j in 2:8)
   {
     if(i%%(j+1) == 0){next}
     for(k in 1:10)
@@ -98,5 +98,42 @@ for(i in 11:30)
     }
   }
 }
+save.image("Q11RegData2")
 
+
+load("Q11RegData")
+library(R2jags)
+mdl <- '
+  model{
+    for(i in 1:2270){
+      logTime[i] ~ dnorm(mu[i],1/vv)
+      mu[i] <- b0 + bstates*states[i]+bactions*actions[i]+bsa*states[i]*actions[i]
+    }
+    vv ~ dgamma(1,.0008)
+    b0 ~ dnorm(0,.0000001)
+    bstates ~ dnorm(0,.0000001)
+    bactions ~ dnorm(0,.0000001)
+    bsa ~ dnorm(0,.0000001)
+  }
+'
+states <- Qdata$States
+actions <- Qdata$Actions
+logTime <- log(Qdata$Time)
+
+#states <- (states - mean(states))/sd(states)#
+#actions <- (actions-mean(actions))/sd(actions)#
+
+data.jags <- c("states","actions","logTime")
+parms <- c("vv","b0","bstates","bactions","bsa")
+writeLines(mdl,"QjagsModel.txt")
+
+Qfit <- jags(data=data.jags,inits = NULL,parameters.to.save = parms,
+             model.file = "QjagsModel.txt",n.chains = 4,
+             n.iter = 12000,n.burnin = 2000,n.thin = 1)
+Qfit
+sims <- as.mcmc(Qfit)
+plot(sims)
+par(mfrow=c(1,1))
+plot(states,logTime)
+plot(actions,logTime)
 
